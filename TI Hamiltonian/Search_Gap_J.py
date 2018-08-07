240930c23f1d77a14712e570ba3c360d6890f5e3
MagShadow <commander.yang.v5@gmail.com>
2018-08-07 13:24:43 -0700

    Plot Gap

--- TI Hamiltonian/Search_Gap_J.py
+++ TI Hamiltonian/Search_Gap_J.py
@@ -4,13 +4,15 @@
 import numpy as np
 import os
 from scipy import linalg
+from matplotlib import pyplot as plt
+
 from TI_Film import Hamiltonian, Eig
 
 
-def main():
-    N_min, N_max, J = 6, 31, 0.00
-    Gap = np.zeros([N_max], dtype=float)
-    for N in range(N_min, N_max):
+def CalcGap():
+    N_min, N_max, J = 6, 30, 0.00
+    Gap = np.zeros([N_max+1], dtype=float)
+    for N in range(N_min, N_max+1):
         S_ = np.array([[1, 0, 0]]*N)
         S = np.array([([s[0]*np.sin(s[1])*np.cos(s[2]), s[0]*np.sin(s[1])
                         * np.sin(s[2]), s[0]*np.cos(s[1])])for s in S_])
@@ -18,8 +20,14 @@ def main():
         eig = np.array([x.real for x in (linalg.eig(h(0, 0))[0])])
         eig.sort()
         Gap[N] = eig[2*N]-eig[2*N-1]
-    print(Gap)
+
+    plt.subplot()
+    plt.plot(list(range(N_min, N_max+1)), Gap[N_min:N_max+1]*1000)
+    plt.xlabel(r"$N$")
+    plt.ylabel(r"$Gap(\rm meV)$")
+    plt.title("Gap vs Thickness, No Spin")
+    plt.savefig("Gap_vs_Thickness_No_Spin.png")
 
 
 if __name__ == "__main__":
-    main()
+    CalcGap()
