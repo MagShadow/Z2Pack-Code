d3f7876dfec49ee8bd2ae21d1fb606189d00885b
MagShadow <commander.yang.v5@gmail.com>
2018-08-08 11:47:06 -0700

    PD Version 1.0

--- TI Hamiltonian/PD_N_J.py
+++ TI Hamiltonian/PD_N_J.py
@@ -3,8 +3,12 @@
 
 import numpy as np
 import os
+import random
 from scipy import linalg
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+
+from json import dumps, loads
 from itertools import product
 from datetime import datetime
 from functools import partial
@@ -14,14 +18,18 @@ from TI_Film import Hamiltonian, Eig
 import TopoInvCalc as TIC
 
 
+def nt():
+    return datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+
+
 def CalcGap():
     N_min, N_max, J = 6, 30, 0.00
     Gap = np.zeros([N_max+1], dtype=float)
     for N in range(N_min, N_max+1):
-        S_ = np.array([[1, 0, 0]]*N)
-        S = np.array([([s[0]*np.sin(s[1])*np.cos(s[2]), s[0]*np.sin(s[1])
-                        * np.sin(s[2]), s[0]*np.cos(s[1])])for s in S_])
-        h = Hamiltonian(N=N, J=J)
+        # S_ = np.array([[1, 0, 0]]*N)
+        # S = np.array([([s[0]*np.sin(s[1])*np.cos(s[2]), s[0]*np.sin(s[1])
+        #                 * np.sin(s[2]), s[0]*np.cos(s[1])])for s in S_])
+        h = Hamiltonian(N=N, J=J)   # By Default it is Spin-Z
         eig = np.array([x.real for x in (linalg.eig(h(0, 0))[0])])
         eig.sort()
         Gap[N] = eig[2*N]-eig[2*N-1]
@@ -53,16 +61,17 @@ def TopoOrder(res, _Chern_tol=0.1):
 def Run(_N, _J, i, j, Phase):
     h = Hamiltonian(N=_N, J=_J)
     res = TIC.Calc(h, CalcZ2=True)
+    Phase[i][j] = TopoOrder(res)
+    # Phase[i][j] = int(random.random()*4)
     # print("New Run!")
     # lock.aquire()
     # print("Get Lock"+datetime.now().strftime("%y-%m-%d-%H-%M-%S"))
-    Phase[i][j] = TopoOrder(res)
     # Phase[i][j] = i+j
-    print("Write:(", i, ",", j, ")=", Phase[i][j])
+    # print("Write:(", i, ",", j, ")=", Phase[i][j])
 
 
 def PhaseDiag():
-    N_min, N_max, J_min, J_max, NJ = 10, 30, 0.00, 0.02, 20
+    N_min, N_max, J_min, J_max, NJ = 15, 25, 0.00, 0.02, 10
     N = np.array(list(range(N_min, N_max+1)), dtype=int)
     # for n in N:
     #     print(type(n))
@@ -71,8 +80,12 @@ def PhaseDiag():
     # global Phase
     # Phase = np.zeros([N_max+1, NJ], dtype=int)
     # lock = Lock()
+
     p, m = Pool(), Manager()
-    Phase = m.list([m.list([0]*NJ)]*(N_max+1))  # 二维数组实在是太坑爹了......
+    Phase = m.list([m.list([0]*NJ) for i in range(N_max-N_min+1)])
+    # 二维数组实在是太坑爹了......
+    # 坑爹*2
+
     # P = [list(x) for x in list(Phase)]
     # P = [list(x) for x in list(Phase)[N_min:N_max+1]]
     # print(P)
@@ -94,10 +107,27 @@ def PhaseDiag():
     p.close()
     p.join()
     # Res=[[]]
-    P = [list(x) for x in list(Phase)[N_min:N_max+1]]
-    plt.subplot()
-    plt.pcolor(x, y, P)
-    plt.savefig("PhaseDiag_N_15_17_J_0_0.05.png")
+    P = [list(x) for x in list(Phase)]
+    # print(P)
+    # pp = [list(x) for x in list(Phase)]
+    # print(pp)
+    stime = nt()
+    with open("PD_Data_"+stime+".txt", "w") as f:
+        d = dict(N_min=N_min, N_max=N_max, J_min=J_min,
+                 J_max=J_max, NJ=NJ, Phase=P)
+        f.write(dumps(d))
+
+    fig, ax = plt.subplots()
+    cmap = mpl.colors.ListedColormap(["r", "g", "b", "c"])
+    norm = mpl.colors.BoundaryNorm(list(range(5)), cmap.N)
+    x, y = np.meshgrid(
+        np.append(N, 2*N[-1]-N[-2]), np.append(J, 2*J[-1]-J[-2]))
+    # print(np.append(J, 2*J[-1]-J[-2]))
+    c = ax.pcolormesh(x, y, np.array(P).T, cmap=cmap, vmin=0, vmax=3)
+    ax2 = fig.add_axes([0.92, 0.1, 0.03, 0.8])
+    cb = mpl.colorbar.ColorbarBase(ax2, cmap=cmap, norm=norm)
+    ax.set_title("Phase Diagram of N & J")
+    plt.savefig("PhaseDiag_"+stime+".png")
     # plt.show()
 
 
--- TI Hamiltonian/TestForMP.py
+++ TI Hamiltonian/TestForMP.py
@@ -1,7 +1,11 @@
 import random
 import time
+import numpy as np
 from functools import partial
 from multiprocessing import Lock, Pool, Manager
+from itertools import product
+from matplotlib import pyplot as plt
+import matplotlib as mpl
 
 
 def Run(i, Y, lock):
@@ -14,23 +18,56 @@ def Run(i, Y, lock):
     # lock.release()
 
 
-def Run_without_lock(i, Y):
-    print("Run: index=", i)
+def Run_without_lock(N_, J_, i, j, Z):
+    # print("Run: index=", i, ",", j)
     # time.sleep(int(random.random()*10))
     # lock.acquire()
     # print("Get Lock!, index=", i)
-    Y[i] = i*i
-    print("Write: index=", i)
+    Z[i][j] = int(random.random()*4)
+    print("i,j,N,J=", i, j, N_, J_)
+    # Z[i][j] = i*10+j
+
+    # print("Write: index=", i, ",", j, " value=", Z[i][j])
+
+
+def Draw(X, Y, Z):
+    # print(X)
+    # print(Y)
+    # print(Z)
+    x, y = np.meshgrid(X, Y, indexing="ij")
+
+    fig, ax = plt.subplots()
+
+    cmap = mpl.colors.ListedColormap(["r", "g", "b", "c"])
+    norm = mpl.colors.BoundaryNorm(list(range(5)), cmap.N)
+    c = ax.pcolormesh(np.array(Z).T, cmap=cmap, vmin=0, vmax=3)
+
+    ax2 = fig.add_axes([0.92, 0.1, 0.03, 0.8])
+
+    cb = mpl.colorbar.ColorbarBase(ax2, cmap=cmap, norm=norm)
+    ax.set_title("Title")
+    # fig.colorbar(cmap, ax=ax, norm=norm)
+    # fig.colorbar(c, ax=ax)
+    # plt.savefig("PhaseDiag_N_15_17_J_0_0.02.png")
+    plt.show()
+    return
 
 
 if __name__ == "__main__":
     p, m = Pool(), Manager()
     # l = m.Lock()
     # global Y
-    Y = m.list([0]*10)
+    N_min, N_max, J_min, J_max, NJ = 15, 20, 0.00, 0.02, 5
+    N = np.array(list(range(N_min, N_max+1)), dtype=int)
+    J = np.linspace(J_min, J_max, num=NJ, endpoint=True)
+
+    x, y = np.meshgrid(N, J, indexing="ij")
+    Z = m.list([m.list([0]*NJ)for i in range(N_max-N_min+1)])
     # pRun = partial(Run, lock=l)
-    for i in range(10):
-        p.apply_async(Run_without_lock, args=(i, Y,))
+    for i, j in product(list(range(N_max-N_min+1)), list(range(NJ))):
+        p.apply_async(Run_without_lock, args=(N[i], J[j], i, j, Z,))
     p.close()
     p.join()
-    print(Y)
+    z = [list(x) for x in list(Z)]
+    print(z)
+    Draw(N, J, z)
