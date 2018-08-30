cc864585235fca3aa8c80b5e55fe6c1a25bde44a
MagShadow <commander.yang.v5@gmail.com>
2018-08-29 20:25:59 -0700

    HZL D=3.3333 Strict

--- TI Hamiltonian/Compare_Paras.py
+++ TI Hamiltonian/Compare_Paras.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from PhaseDiagram import PhaseDiag
-from _utils import nt, convertSpin, settings, settings_strict, CONST_HJZ, CONST_HZL, CONST_HZL_M
+from _utils import nt, convertSpin, settings, settings_strict, CONST_HJZ, CONST_HZL, CONST_HZL_M, CONST_HZL_O
 from TI_Film import Hamiltonian
 from TopoInvCalc import Calc, TopoOrder
 
@@ -40,8 +40,8 @@ def Run_HZL(_N, _J, _i, _j, Phase):
     Use the Parameters from the Hai-Zhou Lu's paper.
     '''
     print("Start Calc: N=%d, J=%.3f" % (int(_N), _J))
-    h = Hamiltonian(N=int(_N), J=_J, Delta=3, **CONST_HZL_M)
-    res = Calc(h, CalcZ2=True, LogOut=False, settings=settings)
+    h = Hamiltonian(N=int(_N), J=_J, Delta=3.333, **CONST_HZL_O)
+    res = Calc(h, CalcZ2=True, LogOut=False, settings=settings_strict)
     Phase[_i][_j] = TopoOrder(res)
 
     print("End Calc: N=%d, J=%.3f; Res=%d" % (int(_N), _J, TopoOrder(res)))
@@ -54,7 +54,7 @@ def Run_HZL(_N, _J, _i, _j, Phase):
 if __name__ == '__main__':
     now = nt()
     # File_0 = "PD_Delta_3_"+now
-    File_1 = "PD_HZL_Origin_"+now
+    File_1 = "PD_HZL_Origin_Strict_"+now
 
     # PD_0 = PhaseDiag().run(Run_0, 6, 20, 15, 0, 0.02, 21, "N", "J")
     # PD_0.write(filename=File_0)
@@ -70,10 +70,10 @@ if __name__ == '__main__':
     #           xlabel="# of Layers", ylabel=r"J(\rm eV)", filename=File_0[:-4])
     # PD_1 = PhaseDiag().run(Run_1, 6, 18, 13, 0, 0.02, 21, "N", "J")
     # PD_1.write(filename=File_1)
-    PD_2 = PhaseDiag().run(Run_HZL, 6, 20, 5, 0, 0.1, 6, "N", "J")
+    PD_2 = PhaseDiag().run(Run_HZL, 6, 18, 5, 0, 0.02, 5, "N", "J")
     PD_2.write(filename=File_1)
 
     # PD_1.draw(title=r"PhaseDiag of N & J, Delta=$3.33\AA$, Spin-z",
     #           xlabel="# of Layers", ylabel=r"J(\rm eV)", filename=File_1)
-    PD_2.draw(title=r"PhaseDiag of N & J, Delta=$3\AA$, Spin-z\nParameters from Hai-Zhou Lu's paper(Modified).",
-              xlabel="# of Layers", ylabel=r"$J(\rm eV)$", filename=File_1)
+    PD_2.draw(title="PhaseDiag of N & J, Delta=$3.333\AA$"+", Spin-z\nParameters from Hai-Zhou Lu's paper.(Strict)",
+              xlabel="# of Layers", ylabel="$J(\rm eV)$", filename=File_1)
